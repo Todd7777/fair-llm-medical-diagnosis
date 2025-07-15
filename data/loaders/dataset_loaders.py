a06e8ca46fd0a4ffe4b413f97739b7e0e68c69a9
tango8 <ethan.b.tang@gmail.com>
2025-07-15 14:05:39 -0400

    continued data processing pipeline

--- data/loaders/dataset_loaders.py
+++ data/loaders/dataset_loaders.py
@@ -1,25 +1,74 @@
 from torch.utils.data import DataLoader
+import pandas as pd
+from PIL import Image
+import os
+from torch.utils.data import Dataset
+
+# If we establish exactly the format of the data, and they are the same across diff. datasets, they can be the same subclass
 
 
-class ChestXRayDataset:
+class ChestXRayDataset(Dataset):
     def __init__(
         self, data_dir, metadata_path, transform, split, demographic_key, label_col
     ):
+        self.data_dir = data_dir
+        self.metadata_path = metadata_path
+        self.transform = transform
+        self.split = split
+        self.demographic_key = demographic_key
+        self.label_col = label_col
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
         pass
 
 
-class PathologyImageDataset:
+class PathologyImageDataset(Dataset):
     def __init__(
         self, data_dir, metadata_path, transform, split, demographic_key, label_col
     ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.metadata_path = metadata_path
+        self.transform = transform
+        self.split = split
+        self.demographic_key = demographic_key
+        self.label_col = label_col
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
         pass
 
 
-class RetinalImageDataset:
+class RetinalImageDataset(Dataset):
     def __init__(
         self, data_dir, metadata_path, transform, split, demographic_key, label_col
     ):
-        pass
+        super().__init__()
+        self.data_dir = data_dir
+        self.metadata = pd.read_csv(metadata_path)
+        self.transform = transform
+        self.split = split
+        self.demographic_key = demographic_key
+        self.label_col = label_col
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+        img_path = os.path.join(self.data_dir, row["path"])
+        image = Image.open(img_path)
+        if self.transform:
+            image = self.transform(image)  # PIL image
+        label = row[self.label_col]
+        demographic = row[self.demographic_key]  # This should mean column
+
+        return {"image": image, "label": label, "demographic": demographic}
 
 
 def create_data_loaders(dataset, batch_size, num_workers=0, shuffle=False):
