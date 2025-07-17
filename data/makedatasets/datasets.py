fccd1e847cddfcfda2ecbb0f18d9b97607aaf5fe
tango8 <ethan.b.tang@gmail.com>
2025-07-16 21:27:30 -0400

    renaming, small updates

--- data/makedatasets/datasets.py
+++ data/makedatasets/datasets.py
@@ -4,9 +4,8 @@ from PIL import Image
 import os
 from torch.utils.data import Dataset
 
-# If we establish exactly the format of the data, and they are the same across diff. datasets, they can be the same subclass
 
-
+# chexpert is already in a dataframe format
 class ChestXRayDataset(Dataset):
     def __init__(
         self, data_dir, metadata_path, transform, split, demographic_key, label_col
@@ -45,35 +44,36 @@ class PathologyImageDataset(Dataset):
 
 
 class RetinalImageDataset(Dataset):
-    def __init__(
-        self, data_dir, metadata_path, transform, split, demographic_key, label_col
-    ):
+    def __init__(self, data_dir, metadata_path, transform, split):
         super().__init__()
         self.data_dir = data_dir
         self.metadata = pd.read_csv(metadata_path)
         self.transform = transform
         self.split = split
-        self.demographic_key = demographic_key
-        self.label_col = label_col
 
     def __len__(self):
         return len(self.metadata)
 
+    # works when the keys are all at the top row, all the info following the same format in rows below; dataframe format
     def __getitem__(self, idx):
         row = self.metadata.iloc[idx]
         img_path = os.path.join(self.data_dir, row["path"])
         image = Image.open(img_path)
         if self.transform:
             image = self.transform(image)  # PIL image
-        label = row[self.label_col]
-        demographic = row[self.demographic_key]  # This should mean column
+        label = row["label"]
+        demographic = row["demographic"]
 
-        return {"image": image, "label": label, "demographic": demographic}
+        return {
+            "image": image,
+            "label": label,
+            "demographic": demographic,
+        }  # def something diff
 
 
-def create_data_loaders(dataset, batch_size, num_workers=0, shuffle=False):
+def create_data_loader(dataset, batch_size, num_workers, shuffle):
     dataloader = DataLoader(
-        dataset,
+        dataset=dataset,
         batch_size=batch_size,
         num_workers=num_workers,
         shuffle=shuffle,
