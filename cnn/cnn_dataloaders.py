a099a770919126481c97520cd052c2bf1418f8e4
tango8 <ethan.b.tang@gmail.com>
2025-07-22 14:38:06 -0400

    functionality for different transform settings on diff cnns

--- cnn/cnn_dataloaders.py
+++ cnn/cnn_dataloaders.py
@@ -8,17 +8,29 @@ import data.makedatasets.datasets as datasets
 
 # can split up into 2 dirs for train and val
 def make_cnn_dataloader(data_args, dataset_class, batch_size):
-    transform = transforms.Compose(  # img preprocess pipeline
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            # transforms.RandomHorizontalFlip(),   # maybe
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            ),  # common values for NNs trained on imagenet
-        ]
-    )
+    transform = None
+    if data_args["model_name"] == "efficientnet":
+        transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                # transforms.RandomHorizontalFlip(),   # maybe
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),  # common values for NNs trained on imagenet
+            ]
+        )
+    elif data_args["model_name"] == "someothernet":
+        transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                # transforms.RandomHorizontalFlip(),   # maybe
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0, 0, 0], std=[0, 0, 0]),
+            ]
+        )
 
     data_args = dict(data_args)  # doesn't edit orig
 
